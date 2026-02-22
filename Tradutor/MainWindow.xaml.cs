d61dbcdd3e796dd081678b94e37144a97b17611f
gabrielosti28 <gabriel.osti@aluno.ifsp.edu.br>
2026-02-22 15:56:31 -0300

    Adicionar arquivos de projeto.

---
+++ Tradutor/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+
+namespace Tradutor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Carrega o Painel de Controle como tela inicial
+            BtnPainelControle_Click(this, null);
+        }
+
+        private void BtnPainelControle_Click(object sender, RoutedEventArgs e)
+        {
+            ConteudoPrincipal.Content = new Views.PainelControleView();
+        }
+
+        private void BtnDrivers_Click(object sender, RoutedEventArgs e)
+        {
+            // Em breve
+            MostrarEmBreve("Gerenciador de Drivers");
+        }
+
+        private void BtnImpressoras_Click(object sender, RoutedEventArgs e)
+        {
+            // Em breve
+            MostrarEmBreve("Gerenciador de Impressoras");
+        }
+
+        private void BtnTerminal_Click(object sender, RoutedEventArgs e)
+        {
+            // Em breve
+            MostrarEmBreve("Terminal Fácil");
+        }
+
+        private void MostrarEmBreve(string modulo)
+        {
+            ConteudoPrincipal.Content = new System.Windows.Controls.TextBlock
+            {
+                Text = $"🚧 O módulo '{modulo}' está sendo construído!\nVolte em breve.",
+                FontSize = 20,
+                TextAlignment = System.Windows.TextAlignment.Center,
+                VerticalAlignment = System.Windows.VerticalAlignment.Center,
+                Foreground = new System.Windows.Media.SolidColorBrush(
+                    System.Windows.Media.Color.FromRgb(100, 130, 160))
+            };
+        }
+    }
+}
---
+++ Tradutor/Views/PainelControleView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Tradutor.Views
+{
+    public partial class PainelControleView : UserControl
+    {
+        public PainelControleView()
+        {
+            InitializeComponent();
+        }
+
+        private void AbrirNomePC_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("SystemPropertiesComputerName.exe");
+        }
+
+        private void AbrirDesempenho_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("ms-settings:about") { UseShellExecute = true });
+        }
+
+        private void AbrirDataHora_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("ms-settings:dateandtime") { UseShellExecute = true });
+        }
+
+        private void AbrirProgramas_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("ms-settings:appsfeatures") { UseShellExecute = true });
+        }
+
+        private void AbrirAtualizacoes_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("ms-settings:windowsupdate") { UseShellExecute = true });
+        }
+
+        private void AbrirRede_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("ms-settings:network") { UseShellExecute = true });
+        }
+    }
+}
